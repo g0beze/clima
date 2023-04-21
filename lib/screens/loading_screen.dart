2138be760143537ef2ab95294887f30430cb519e
MENDEX <39565143+M3ND3X@users.noreply.github.com>
2023-04-21 07:04:08 +0300

    setup http package and initalized an API call with response

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -20,8 +21,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+      Uri.parse(
+          'https://samples.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=b6907d289e10d714a6e88b30761fae22'),
+    );
+    String? data;
+    (response.statusCode == 200)
+        ? (data = response.body)
+        : print(response.statusCode);
+    print(data);
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
